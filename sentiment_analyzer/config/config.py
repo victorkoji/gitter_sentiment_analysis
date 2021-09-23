e5934bd5480e59acdb024a659d52cb60590cf197
Victor Koji <vitorkoji15@gmail.com>
2021-09-22 22:43:43 -0400

    Alterando pasta de dados

--- sentiment_analyzer/config/config.py
+++ sentiment_analyzer/config/config.py
@@ -2,10 +2,10 @@ class Config:
 
 	def __init__(self, folder_name, filename):
 
-		self.path_chat_room = f"../chat_rooms/{folder_name}/{filename}/{filename}"
-		self.path_chat_room_with_prefix = f"../chat_rooms/{folder_name}/{filename}/{filename}"
-		self.path_graphs = f"../chat_rooms/{folder_name}/{filename}/graphs"
-		self.path_query_result = f"../chat_rooms/{folder_name}/{filename}/query_result"
+		self.path_chat_room = f"../data/chat_rooms/{folder_name}/{filename}/{filename}"
+		self.path_chat_room_with_prefix = f"../data/chat_rooms/{folder_name}/{filename}/{filename}"
+		self.path_graphs = f"../data/chat_rooms/{folder_name}/{filename}/graphs"
+		self.path_query_result = f"../data/chat_rooms/{folder_name}/{filename}/query_result"
 
 	def get_path_chat_room(self) -> str:
 		return self.path_chat_room
--- sentiment_analyzer/src/query/get_quantity_messages.py
+++ sentiment_analyzer/src/query/get_quantity_messages.py
@@ -1,29 +1,48 @@
 import pandas as pd
 import numpy as np
 import os
+from config.config import Config
 
 # Print quantity median messages for each folder theme.
 class QuantityMessages:
 
+    def __init__(self):
+        self.path_folder_general = f"../data/general_information"
+        self.file_path = f"../data/chat_rooms"
+
     def process(self):
         
-        # project_median = {}
+        project_chatroom_messages_median = {}
         project_quantity_message = {}
 
-        for folder_name in os.listdir("../chat_rooms"):
+        # Create path to folder
+        if not os.path.exists(f"{self.path_folder_general}/quantity_messages_chatroom"):
+            os.makedirs(f"{self.path_folder_general}/quantity_messages_chatroom")
+
+        for folder_name in os.listdir(self.file_path):
 
             project_quantity_message[folder_name] = {}
 
-            for filename in os.listdir(f"../chat_rooms/{folder_name}"):
-                df = pd.read_csv(fr"../chat_rooms/{folder_name}/{filename}/{filename}.csv",  encoding='utf-8', sep = '|')
+            # Iterate the projects to get the total amount of messages
+            for filename in os.listdir(f"{self.file_path}/{folder_name}"):
+                df = pd.read_csv(fr"{self.file_path}/{folder_name}/{filename}/{filename}.csv",  encoding='utf-8', sep = '|')
 
                 project_quantity_message[folder_name][filename] = len(df)
 
         for folder_theme, theme in project_quantity_message.items():
             median = np.median(list(theme.values()))
+
             pd_median = pd.DataFrame(theme.items(), columns=['chat_room', 'quantity_messages'])
             pd_median = pd_median.sort_values(by=['quantity_messages'], ascending=False)
-            project_selected = pd_median[pd_median['quantity_messages'] >= median]
             
-            print(f"{folder_theme} quantity messages median: ", np.median(list(theme.values())))
-            print(project_selected)
+            project_selected = pd_median[pd_median['quantity_messages'] >= median]
+
+            # Save the paths of each thread id in a CSV file
+            project_selected.to_csv(f"{self.path_folder_general}/quantity_messages_chatroom/{folder_theme}_relation_messages.csv", encoding='utf-8', index=False, sep = '|')
+            
+            # Save theme and median
+            project_chatroom_messages_median[folder_theme] = median
+
+        
+        df_chatroom_median = pd.DataFrame(project_chatroom_messages_median.items(), columns=['chat_room', 'messages_median'])
+        df_chatroom_median.to_csv(f"{self.path_folder_general}/quantity_messages_chatroom/chatroom_messages_median.csv", encoding='utf-8', index=False, sep = '|')
