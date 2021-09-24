62768c932b15139ee8cac022f07ed6267abe9d1f
Victor Koji <vitorkoji15@gmail.com>
2021-09-23 23:46:14 -0400

    Github - Caso nao seja possivel criar o csv, remover o arquivo

--- consuming_api_github/src/util.py
+++ consuming_api_github/src/util.py
@@ -1,4 +1,4 @@
-import requests, json, os.path
+import requests, json, os.path, csv
 import pandas as pd
 from src.config import Config
 class Util:
@@ -30,4 +30,18 @@ class Util:
 		print(path)
 		df = pd.read_json(path, typ='series')
 		path_csv = path.replace(".json", ".csv")
-		df.to_csv(path_csv, encoding='utf-8', index=False, sep="|")
+
+		try:
+			with open(path_csv, "w") as f:
+				wr = csv.DictWriter(f, delimiter="|", fieldnames=list(df[0].keys()), extrasaction='ignore')
+				wr.writeheader()
+				wr.writerows(df)
+		except:
+			# If an error occurred, remove the file csv 
+			if os.path.exists(path_csv):
+				os.remove(path_csv)
+
+			print("Não foi possível converter para csv.")
+
+
+		# df.to_csv(path_csv, encoding='utf-8', index=False, sep="|", header=True)
